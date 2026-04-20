5110d3315592fdb243aa68bfb1785d7f07eb51b1
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 07:30:41 -0400

    Create blocks.js

---
+++ blocks/blocks/blocks.js
@@ -0,0 +1,6 @@
+function getBlockColor(type) {
+    if (type === BLOCK_TYPES.GRASS) return "#3cb043";
+    if (type === BLOCK_TYPES.DIRT) return "#8b5a2b";
+    if (type === BLOCK_TYPES.STONE) return "#777";
+    return "#87ceeb";
+}
